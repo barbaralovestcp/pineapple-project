0c06a815203074ba6564bd22da44e92e7e71c0b1
Lea <lea_chemoul@orange.fr>
2019-04-01 09:20:59 +0200

    smtp code and commands

---
+++ src/org/pineapple/CodeDATASMTP.java
@@ -0,0 +1,61 @@
+package org.pineapple;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CodeDATASMTP extends Code implements ParametrizedToString {
+    public enum CodeEnum implements ICodeEnum {
+        START(parameter -> "Start mail input; end with <CRLF>.<CRLF>");
+
+        @NotNull
+        private ICodeEnum toString;
+
+        CodeEnum(ICodeEnum toString) {
+            setToString(toString);
+        }
+
+        @NotNull
+        @Contract(pure = true)
+        private ICodeEnum getToString() {
+            return toString;
+        }
+
+        private void setToString(@NotNull ICodeEnum toString) {
+            this.toString = toString;
+        }
+
+        @NotNull
+        @Override
+        public String toString(@Nullable String parameter) {
+            return toString.toString(parameter);
+        }
+        @NotNull
+        @Override
+        public String toString() {
+            return toString.toString(null);
+        }
+    }
+
+    public CodeDATASMTP(ICodeEnum codeType) {
+        super(codeType);
+    }
+    public CodeDATASMTP() {
+        super(CodeERRSMTP.CodeEnum.ERR);
+    }
+
+    /* OVERRIDES */
+
+    @NotNull
+    @Override
+    public String toString(@Nullable String parameter) {
+        String message = getCodeType().toString(parameter);
+        return "354 " + (message.length() > 0 ? " " + message : "") + "\r\n";
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return toString(null);
+    }
+}
---
+++ src/org/pineapple/CodeERRSMTP.java
@@ -0,0 +1,64 @@
+package org.pineapple;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CodeERRSMTP extends Code implements ParametrizedToString{
+
+    public enum CodeEnum implements ICodeEnum {
+        ERR(parameter -> ""),
+        ERR_INVALID_RCPT(parameter -> "No sush user here"),
+        ;
+
+        @NotNull
+        private ICodeEnum toString;
+
+        CodeEnum(ICodeEnum toString) {
+            setToString(toString);
+        }
+
+        @NotNull
+        @Contract(pure = true)
+        private ICodeEnum getToString() {
+            return toString;
+        }
+
+        private void setToString(@NotNull ICodeEnum toString) {
+            this.toString = toString;
+        }
+
+        @NotNull
+        @Override
+        public String toString(@Nullable String parameter) {
+            return toString.toString(parameter);
+        }
+        @NotNull
+        @Override
+        public String toString() {
+            return toString.toString(null);
+        }
+    }
+
+    public CodeERRSMTP(ICodeEnum codeType) {
+        super(codeType);
+    }
+    public CodeERRSMTP() {
+        super(CodeERRSMTP.CodeEnum.ERR);
+    }
+
+    /* OVERRIDES */
+
+    @NotNull
+    @Override
+    public String toString(@Nullable String parameter) {
+        String message = getCodeType().toString(parameter);
+        return "550 " + (message.length() > 0 ? " " + message : "") + "\r\n";
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return toString(null);
+    }
+}
