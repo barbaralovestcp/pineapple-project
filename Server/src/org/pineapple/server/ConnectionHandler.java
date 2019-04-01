b9f622919cfb8c27cc16e8955d974ebdf6dba680
Valentin Berger <Valentin.berger38@gmail.com>
2019-04-01 08:53:26 +0200

    Started generification

--- Server/src/org/pineapple/server/ConnectionHandler.java
+++ Server/src/org/pineapple/server/ConnectionHandler.java
@@ -3,14 +3,14 @@ package org.pineapple.server;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.pineapple.AbstractServerConnectionHandler;
 import org.pineapple.Message;
 import org.pineapple.stateMachine.Context;
 import org.pineapple.stateMachine.InputStateMachine;
 
-import java.io.*;
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.Function;
@@ -20,28 +20,15 @@ import java.util.function.Function;
  * This class has been made with the help of Stack Overflow community
  * Source: https://stackoverflow.com/questions/28629669/java-tcp-simple-webserver-problems-with-response-codes-homework?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
  */
-public class ConnectionHandler implements Runnable {
+public class ConnectionHandler extends AbstractServerConnectionHandler implements Runnable {
 
-    @NotNull
-	private Socket so_client;
-	private InputStreamReader in_data;
-	private PrintStream out_data;
+	private String messToLog;
 	@NotNull
 	private Context context;
-	private String messToLog;
-
-	@Nullable
-	private Function<String, Void> onLog;
 
 	public ConnectionHandler(@NotNull Socket so_client, @Nullable Function<String, Void> onLog) throws IOException {
-		setClient(so_client);
-
-		in_data = new InputStreamReader(so_client.getInputStream(), StandardCharsets.ISO_8859_1);
-		out_data = new PrintStream(so_client.getOutputStream());
+		super(so_client, onLog);
 		context = new Context(new StateServerListening());
-		setOnLog(onLog);
-
-		tryLog("New connection: " + getClientName());
 	}
 	public ConnectionHandler(@NotNull Socket so_client) throws IOException {
 		this(so_client, null);
@@ -112,20 +99,7 @@ public class ConnectionHandler implements Runnable {
 
 	}
 
-	public String getClientName() {
-		return so_client.getInetAddress().getHostAddress() + ":" + so_client.getPort() + " (\"" + so_client.getInetAddress().getCanonicalHostName() + "\")";
-	}
-
 	/* CONNECTION HANDLER METHODS */
-
-	public void sendMessage(@NotNull String message){
-		if(out_data != null) {
-			out_data.print(message.replaceAll("\\r(?!\\n)", "\r\n"));
-			System.out.println(String.format("%040x", new BigInteger(1, message.getBytes(StandardCharsets.ISO_8859_1))));
-			out_data.flush();
-			tryLog("Command sent");
-		}
-	}
 
 	//Send a message of the mailbox
 	public void sendMailBoxMessage( @NotNull String command, @NotNull String message){
@@ -140,37 +114,6 @@ public class ConnectionHandler implements Runnable {
 						.buildMessage());
 			out_data.flush();
 			tryLog("Message sent");
-		}
-	}
-
- 	@Nullable
-	public byte[] getFileData(@NotNull File file) {
-		FileInputStream in = null;
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		byte[] raw = new byte[4096];
-		int size;
-
-		try {
-			in = new FileInputStream(file);
-
-			while ((size = in.read(raw)) >= 0)
-				baos.write(raw, 0, size);
-
-			baos.flush();
-			baos.close();
-
-			return baos.toByteArray();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return null;
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
