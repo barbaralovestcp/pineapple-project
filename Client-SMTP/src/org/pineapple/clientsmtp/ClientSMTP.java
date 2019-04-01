787b91c7296e5cd6d24c782619c53fe53225f715
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2019-04-01 09:56:33 +0200

    add observer to interface

--- Client-SMTP/src/org/pineapple/clientsmtp/ClientSMTP.java
+++ Client-SMTP/src/org/pineapple/clientsmtp/ClientSMTP.java
@@ -1,4 +1,17 @@
 package org.pineapple.clientsmtp;
 
 public class ClientSMTP {
+
+    private boolean isConnected;
+    private String serverMessage;
+
+    //TODO : notify observer à chaque réception d'un message du serveur
+
+    boolean isConnected() {
+        return isConnected;
+    }
+
+    public String getServerMessage() {
+        return serverMessage;
+    }
 }
