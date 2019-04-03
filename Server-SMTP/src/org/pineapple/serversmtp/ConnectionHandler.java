a2254d6ddaa90096657b1f33729f49da1bb6bc2b
Valentin Berger <Valentin.berger38@gmail.com>
2019-04-03 11:56:12 +0200

    Done generifying

--- Server-SMTP/src/org/pineapple/serversmtp/ConnectionHandler.java
+++ Server-SMTP/src/org/pineapple/serversmtp/ConnectionHandler.java
@@ -3,13 +3,16 @@ package org.pineapple.serversmtp;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.pineapple.AbstractServerConnectionHandler;
+import org.pineapple.CommandSMTP;
+import org.pineapple.ICommand;
+import org.pineapple.stateMachine.AbstractInputStateMachine;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.function.Function;
 
-public class ConnectionHandler extends AbstractServerConnectionHandler<InputStateMachineSMTP> {
+public class ConnectionHandler extends AbstractServerConnectionHandler<CommandSMTP> {
 	
 	public ConnectionHandler(@NotNull Socket so_client, @Nullable Function<String, Void> onLog) throws IOException {
 		super(so_client, onLog, new StateListening(), InputStateMachineSMTP.class, InputStateMachineSMTP::new);
