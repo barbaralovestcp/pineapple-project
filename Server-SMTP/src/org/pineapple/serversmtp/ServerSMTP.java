a0384f1a2e923418d2146c3f4d86aee5343c31c8
Valentin Berger <Valentin.berger38@gmail.com>
2019-04-23 19:15:09 +0200

    Disable encryption for SMTP
    * The encryption is disable for Server-SMTP.
    * Reseted the MBX files.

--- Server-SMTP/src/org/pineapple/serversmtp/ServerSMTP.java
+++ Server-SMTP/src/org/pineapple/serversmtp/ServerSMTP.java
@@ -3,6 +3,7 @@ package org.pineapple.serversmtp;
 import org.jetbrains.annotations.NotNull;
 import org.pineapple.AbstractServer;
 import org.pineapple.AbstractServerConnectionHandler;
+import org.pineapple.MBXManager;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -11,6 +12,8 @@ public class ServerSMTP extends AbstractServer implements Runnable {
 	
 	public ServerSMTP() {
 		super(25);
+		// Disable MD5/AES file encryption for the SMTP server, in order to read the mail as text files.
+		MBXManager.setUseEncryption(false);
 	}
 	
 	public static void main(String[] args) {
